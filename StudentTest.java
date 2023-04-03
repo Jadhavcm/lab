f717e8ba03dfb439bac29975d89a18c00804b532
Jadhavcm <127472247+Jadhavcm@users.noreply.github.com>
2023-04-03 10:13:10 +0530

    Add files via upload

---
+++ StudentTest.java
@@ -0,0 +1,11 @@
+package Labwork30march;
+
+ class StudentTest {
+	public static void main(String[] args) {
+		
+	Student student1 = new Student("Ajay", 25, "AGPIT");
+
+	System.out.println(student1.getName() + " is " + student1.getAge() + " years old, and the collage name is " + student1.getcollage() + ".");
+
+}
+}
